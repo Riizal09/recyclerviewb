4291093e51de171d3d7167be7e63683d40aabead
Riizal09 <rizalfadilah0908@gmail.com>
2023-06-15 11:36:56 +0700

    f
    first commit

---
+++ app/src/main/java/com/rizalfadilah/recyclerviewb/AsyncHttpsClient.java
@@ -0,0 +1,6 @@
+package com.rizalfadilah.recyclerviewb;
+
+import com.loopj.android.http.AsyncHttpClient;
+
+public class AsyncHttpsClient extends AsyncHttpClient {
+}
---
+++ app/src/main/java/com/rizalfadilah/recyclerviewb/MahasiswaModel.java
@@ -0,0 +1,41 @@
+package com.rizalfadilah.recyclerviewb;
+
+public class MahasiswaModel
+{
+    private String NIM, nama,  JenisKelamanin, JP;
+
+    public MahasiswaModel(){
+    }
+
+    public String getNIM() {
+        return NIM;
+    }
+
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenisKelamanin() {
+        return JenisKelamanin;
+    }
+
+    public void setJenisKelamanin(String jenisKelamanin) {
+        JenisKelamanin = jenisKelamanin;
+    }
+
+    public String getJP() {
+        return JP;
+    }
+
+    public void setJP(String JP) {
+        this.JP = JP;
+    }
+}
